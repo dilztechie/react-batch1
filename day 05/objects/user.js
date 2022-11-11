f087e75d3e8acec9eb0cb45b1c630ac204c45ef1
Dilz <mail2dilz@gmail.com>
2022-11-11 10:30:37 +0530

    Add files via upload

---
+++ day 05/objects/user.js
@@ -0,0 +1,11 @@
+class User {
+    // members of the class: properties
+    username
+    password
+
+    // constructor
+    constructor(username, password) {
+        this.username = username
+        this.password = password
+    }
+}
