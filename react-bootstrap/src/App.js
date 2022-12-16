bae3f86b756526ad81619f3b7ee45c2bee66cd84
Dilz <mail2dilz@gmail.com>
2022-12-16 10:33:56 +0530

    Add files via upload

---
+++ react-bootstrap/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Banner from './components/banner/banner';
+import Form from './components/form/form';
+
+function App() {
+  return (<div className="App">
+    <Banner />
+    <Form />
+  </div>)
+}
+
+export default App
